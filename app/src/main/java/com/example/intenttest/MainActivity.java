c4530dbce0ef2c52295ba7164ddac148a9cde902
ToukaiAkihiro <tokaimas@yahoo.co.jp>
2015-06-13 23:46:16 +0900

    Delete:UnusedCode

--- app/src/main/java/com/example/intenttest/MainActivity.java
+++ app/src/main/java/com/example/intenttest/MainActivity.java
@@ -52,14 +52,6 @@ public class MainActivity extends Activity {
             uiEditText.setText(sub+""+ text);
         }
 
-/*
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.main, menu);
-            return true;
-        }
-    }*/
 
     }
 }
